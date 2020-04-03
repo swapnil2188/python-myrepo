6fe0391f1f93d790815a84c67f2d1e68b4f70e7a
sshevate <sshevate@apple.com>
2020-04-02 20:06:48 -0700

    Update new scripts
    Reviewed by: swapnil2188
    Approved by: swapnil2188

---
+++ acloudguru/seconds_in_a_day.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+
+day = 1
+hour = 24
+minutes = 60
+seconds = 60
+
+total_secs = int(day) * int(hour) * int(minutes) * int(seconds)
+
+print (total_secs)
---
+++ acloudguru/shopping-cart-3.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+
+s = 6
+
+if (s < 3):
+	print ("Less than 3 Buy more\n")
+elif s >= 3 and s <= 5:
+	print ("Thanks\n")
+elif (s > 5):
+	print ("Discount\n")
